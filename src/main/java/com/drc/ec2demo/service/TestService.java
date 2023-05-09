ac4cca7cb738eea961f10d9ca24aeac64e4aa65d
Balasubramanian <balasubramaniants99@gmail.com>
2023-05-09 12:05:49 +0530

    Initial commit for aws-ec2 demo spring boot project

---
+++ src/main/java/com/drc/ec2demo/service/TestService.java
@@ -0,0 +1,11 @@
+package com.drc.ec2demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test() {
+        return "Response from Test !";
+    }
+}
