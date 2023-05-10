7076a341c762d9a4db80f664b215f5a90164ea67
Balasubramanian <balasubramaniants99@gmail.com>
2023-05-10 14:58:36 +0530

    Changes in deployment shell file

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ yes | sudo apt install openjdk-17-jdk
 echo 'Installing apt-transport...'
 yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
 yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 echo 'Installing docker-ce...'
 yes | sudo apt install docker-ce
