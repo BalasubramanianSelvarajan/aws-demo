9baf8cc133bcfb385ed2e45e24dcfc169539b2ad
Balasubramanian <balasubramaniants99@gmail.com>
2023-05-11 20:53:04 +0530

    added pre-deploy script and workflow changes

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -5,7 +5,7 @@ echo '------------------ Starting Pre-Install ------------------'
 # Install required dependencies
 yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
 yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 yes | sudo apt install docker-ce
 
